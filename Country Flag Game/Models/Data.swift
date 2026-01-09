13273e169e718a6999c81d9ff68e5c2312a01651
jkeele <jkeele@bsd220.org>
2026-01-09 10:18:46 -0600

    added array of country flag names

---
+++ Country Flag Game/Models/Data.swift
@@ -0,0 +1,33 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Jensen Keele on 1/8/26.
+//
+
+import Foundation
+
+struct Data {
+    let countries = [
+        "Angola",
+        "Austria",
+        "China",
+        "England",
+        "Estonia",
+        "France",
+        "Germany",
+        "Greece",
+        "India",
+        "Israel",
+        "Italy",
+        "Norway",
+        "Pakistan",
+        "Peru",
+        "Saudi Arabia",
+        "South Africa",
+        "Spain",
+        "Sweden",
+        "Ukraine",
+        "United States"
+    ]
+}
---
+++ Country Flag Game/Views/QuestionView.swift
@@ -0,0 +1,43 @@
+//
+//  QuestionView.swift
+//  Country Flag Game
+//
+//  Created by Jensen Keele on 1/8/26.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        VStack (spacing:30){
+            HStack {
+                Text("Country Flag Game")
+                    .fontWeight(.heavy)
+                    .padding()
+                Spacer()
+                Text("1 out of 3")
+                    .padding()
+            }
+            ProgressBar(progress: 78)
+            VStack(spacing: 10) {
+                Text("Which country's flag is this?")
+                    .font(.title)
+                Image("Italy")
+                AnswerRow(answer: Answer(text: "Italy", isCorrect: true))
+                AnswerRow(answer: Answer(text: "England", isCorrect: false))
+                AnswerRow(answer: Answer(text: "Germany", isCorrect: false))
+                AnswerRow(answer: Answer(text: "France", isCorrect: false))
+            }
+            CustomButton(text: "Next")
+            Spacer()
+        }
+        .foregroundColor(.yellow)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.cyan)
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+#Preview {
+    QuestionView()
+}
